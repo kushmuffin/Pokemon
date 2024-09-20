68422cb9ccec64ede4c3c01f3fb97e2ea9ca7a64
kushmuffin <kushlee24@gmail.com>
2024-09-21 00:29:02 +0800

    忽略 vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // vite環境下 推上github page需要這行 本地端開發須備註
-  // base: "/Pokemon/",
-})
